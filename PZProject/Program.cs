9c8ef1c35e9b62bdc3ce57a33ba23c26e57ee0d8
Julia <62382551+juliazhorniakAMI@users.noreply.github.com>
2021-12-06 11:28:40 +0200

    Add files via upload

---
+++ PZProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using CursovaApp.BLL.Services.Abstract;
+using CursovaApp.DAL.Repositories;
+using CursovaApp.Models;
+using CursovaApp.Repositories;
+using PZProject;
+using PZProject.BLL.Services.Abstract;
+using PZProject.BLL.Services.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CursovaApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // Menu m = new Menu();
+        
+        }
+    }
+}
