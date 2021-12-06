89923f401282375a8bc2cfe59434aca2d4d8d788
Julia <62382551+juliazhorniakAMI@users.noreply.github.com>
2021-12-06 11:31:52 +0200

    Add files via upload

---
+++ PZProject.DTO/ModelsDTO/CustomerDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CursovaApp.Models
+{
+    public partial class CustomerDTO
+    {
+
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string Pass { get; set; }
+        public virtual RoleDTO Role { get; set; }
+        public Guid Salt { get; set; }
+    }
+}
---
+++ PZProject.DTO/ModelsDTO/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CursovaApp.Models
+{
+    public partial class OrderDTO
+    {
+      
+        public int Id { get; set; }
+        public int? CustomerId { get; set; }
+       public DateTime DateOfCreation { get; set; }
+        public virtual CustomerDTO Customer { get; set; }
+        public virtual ICollection<OrderDetailDTO> OrderDetails { get; set; }
+
+    }
+}
